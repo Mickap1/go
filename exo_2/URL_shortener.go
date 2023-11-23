2f2396074e6ffa8bc430959b29746fd91af22ff8
Mickap1 <mickael1.peireto@epitech.eu>
2023-11-23 11:02:47 +0100

    Working URL Shortener

--- exo_2/URL_shortener.go
+++ exo_2/URL_shortener.go
@@ -8,7 +8,16 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-type MyHandler struct{}
+//Create an interface with short and long in url
+
+type UrlsWrapper struct {
+	Urls []Url `yaml:"urls"`
+}
+
+type Url struct {
+	Short string
+	Long  string
+}
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -31,28 +40,30 @@ func YAMLHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	return MapHandler(pathMap, fallback), nil
 }
 
-func buildMap(parsedYaml map[string]string) map[string]string {
+func buildMap(urls UrlsWrapper) map[string]string {
 	pathMap := make(map[string]string)
-	for key, value := range parsedYaml {
-		pathMap[key] = value
+	for key, value := range urls.Urls {
+		pathMap[value.Short] = value.Long
+		// fmt.Println(key, value)
 	}
 	return pathMap
 }
 
-func parseYAML(yamlData []byte) (map[string]string, error) {
-	var data map[string]string
-	// Var_name map[]
-	err := yaml.Unmarshal(yamlData, &data)
+func parseYAML(yamlData []byte) (UrlsWrapper, error) {
+	//List of url structs
+	var urls UrlsWrapper
+
+	// fmt.Print(string(yamlData))
+	err := yaml.Unmarshal(yamlData, &urls)
 	if err != nil {
-		return nil, err
+		fmt.Print("An error occured")
+		return urls, err
 	}
-
-	// DEBUG
-	for short, long := range data {
-		fmt.Printf("Short: %s, Long: %s\n", short, long)
-	}
-
-	return data, nil
+	// //Display the content of urls DEBUG
+	// for i := 0; i < len(urls.Urls); i++ {
+	// 	fmt.Println(urls.Urls[i].Short, urls.Urls[i].Long)
+	// }
+	return urls, nil
 }
 
 func main() {
@@ -60,7 +71,6 @@ func main() {
 	if err_i != nil {
 		panic(err_i)
 	}
-	fmt.Print(string(yamlFile))
 	parsedYaml, err := parseYAML(yamlFile)
 	if err != nil {
 		panic(err)
